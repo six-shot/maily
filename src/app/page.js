2b7e0dc54d39dd73082445445a96ebb816324337
six-shot <okhuomonajayi54@gmail.com>
2025-04-17 10:19:39 +0100

    feautures

--- src/app/page.js
+++ src/app/page.js
@@ -10,6 +10,7 @@ import blur from "../../public/Blur.png";
 import right from "../../public/right.svg";
 import left from "../../public/left.svg";
 import mail1 from "../../public/mail1.avif";
+
 export default function Home() {
   return (
     <div>
@@ -50,11 +51,10 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="relative z-10">
-          {" "}
+        <div className="fixed z-[1000]">
           <Navbar />
         </div>
-        <div className=" pl-[14px] pr-5 h-[68px] bg-white rounded-[100px] flex gap-2.5 items-center mt-[99px] relative z-10">
+        <div className=" pl-[14px] pr-5 h-[68px] bg-white rounded-[100px] flex gap-2.5 items-center mt-[188px] relative z-10">
           <div className="flex items-center group">
             <Image
               src={one}
@@ -101,6 +101,90 @@ export default function Home() {
             </button>
             <button className="h-[65px] px-[60px] explore-btn text-[22px] font-medium font-[family-name:var(--font-inter)] text-black">
               𝕏 Reach us
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="max-w-[1440px] mx-auto px-[48px]">
+        <div className="mt-[90px] flex justify-between">
+          <h2 className="text-[56px] font-[family-name:var(--font-inter)] text-[rgba(0,0,0,0.94)] font-bold leading-[56px]">
+            Features you <br /> will love.
+          </h2>
+          <div>
+            <p className="text-[17px] font-[family-name:var(--font-inter)] text-[black]/70 leading-[27px] w-[351px] text-right">
+              Manage emails for over 5,000 startups all over the world, making
+              the process
+            </p>
+            <p className="text-[17px] font-[family-name:var(--font-inter)] text-[black]/70 leading-[27px] w-[351px] text-right ">
+              seamless and efficient
+            </p>
+          </div>
+        </div>
+        <div className="grid grid-cols-3 gap-6 mt-[56px]">
+          <div className="p-2 border border-white rounded-[50px] shadow-[0_1px_1px_#0000001a,_0_1.5px_1.5px_#00000017,_0_4px_2.5px_#0000000d]">
+            <div className="w-full card-bg h-[258px]"></div>
+            <div className="py-[33px] px-4">
+              <p className="text-[15px] font-[family-name:var(--font-inter)] text-[black]/50 leading-[22px]">
+                We ensure every message stays{" "}
+                <span className="text-black">
+                  {" "}
+                  secure with advanced encryption{" "}
+                </span>
+                , giving you full control over who sees what and when.
+              </p>
+            </div>
+          </div>
+          <div className="p-2 border border-white rounded-[50px] shadow-[0_1px_1px_#0000001a,_0_1.5px_1.5px_#00000017,_0_4px_2.5px_#0000000d]">
+            <div className="w-full card-bg h-[258px]"></div>
+            <div className="py-[33px] px-4">
+              <p className="text-[15px] font-[family-name:var(--font-inter)] text-[black]/50 leading-[22px]">
+                Never miss an{" "}
+                <span className="text-black">important message</span>. Get
+                notified instantly, ensuring{" "}
+                <span className="text-black">you’re always in the loop</span>{" "}
+                and ready.
+              </p>
+            </div>
+          </div>
+          <div className="p-2 border border-white rounded-[50px] shadow-[0_1px_1px_#0000001a,_0_1.5px_1.5px_#00000017,_0_4px_2.5px_#0000000d]">
+            <div className="w-full card-bg h-[258px]"></div>
+            <div className="py-[33px] px-4">
+              <p className="text-[15px] font-[family-name:var(--font-inter)] text-[black]/50 leading-[22px]">
+                Identify errors with{" "}
+                <span className="text-black">
+                  {" "}
+                  smart notifications and search tools
+                </span>
+                , helping you stay organized and solve problems{" "}
+                <span className="text-black">quickly</span>.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="w-full h-[720px] category-bg mt-[90px] flex justify-center items-center flex-col font-[family-name:var(--font-inter)] gap-[14px] relative">
+          <h5 className="text-[22px] text-black/30 font-black">AI-TAGS</h5>
+          <h3 className="text-[rgba(0,0,0,0.94)] text-[56px] font-bold leading-[56px] text-center">
+            Categorization?
+            <br /> Done for you!
+          </h3>
+          <div className="absolute top-[74px] left-[145px]">
+            <button className="flex items-center gap-[17px] design-btn px-[38px] h-[102px] text-[36px] font-extrabold text-[rgba(255,255,255,0.87)] rotate-[-2deg]">
+              👨‍🎨 Design
+            </button>
+          </div>
+          <div className="absolute top-[68px] right-[107px]">
+            <button className="flex items-center gap-[17px] family-btn px-[38px] h-[102px] text-[36px] font-extrabold text-[rgba(255,255,255,0.87)] rotate-[10deg]">
+              ✨ Family
+            </button>
+          </div>
+          <div className="absolute bottom-[52px] left-[94px]">
+            <button className="flex items-center gap-[17px] it-team-btn px-[38px] h-[102px] text-[36px] font-extrabold text-[rgba(255,255,255,0.87)] rotate-[6deg]">
+              👨‍💻 IT Team
+            </button>
+          </div>
+          <div className="absolute bottom-[110px] right-[146px]">
+            <button className="flex items-center gap-[17px] work-btn px-[38px] h-[102px] text-[36px] font-extrabold text-[rgba(255,255,255,0.87)] rotate-[6deg]">
+              💼 Work
             </button>
           </div>
         </div>
