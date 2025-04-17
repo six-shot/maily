b2e8f05ca680dcfce2b0808a9958c3ec84643ab1
six-shot <okhuomonajayi54@gmail.com>
2025-04-17 02:13:04 +0100

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -5,15 +5,25 @@ import Navbar from "@/components/ui/global/navbar";
 import one from "../../public/first.png";
 import second from "../../public/second.png";
 import third from "../../public/third.png";
+import mask from "../../public/mask.svg";
+import blur from "../../public/Blur.png";
 export default function Home() {
   return (
     <div>
-      {/* <Image className="" src={svg} alt="Next.js logo" /> */}
-      {/* 
-      <Image className="w-full   h-[847px] rounded-b-[100px]" src={bg2} alt="bg2" /> */}
-      <div className="hero-bg bg">
-        <Navbar />
-        <div className=" pl-[14px] pr-5 h-[68px] bg-white rounded-[100px] flex gap-2.5 items-center mt-[99px]">
+    
+      <div className="hero-bg h-[847px] bg relative">
+     
+        <div className="absolute -top-[325px] w-[1558px] h-[1558px] border border-[rgba(255,255,255,0.59)] rounded-full flex justify-center items-center blur-[1px]">
+          <div className="w-[1223px] h-[1223px] border border-[rgba(255,255,255,0.59)] rounded-full flex justify-center items-center">
+            <div className="w-[904px] h-[904px] border border-[rgba(255,255,255,0.59)] rounded-full  "></div>
+          </div>
+        </div>
+
+        <div className="relative z-10">
+          {" "}
+          <Navbar />
+        </div>
+        <div className=" pl-[14px] pr-5 h-[68px] bg-white rounded-[100px] flex gap-2.5 items-center mt-[99px] relative z-10">
           <div className="flex items-center">
             <Image
               src={one}
@@ -35,23 +45,25 @@ export default function Home() {
             6,000+ people use our product
           </h6>
         </div>
-        <h1 className="mt-[38px] font-[family-name:var(--font-montserrat)] text-[90px] text-[rgba(255,255,255,0.94)] font-black leading-[90px] text-center">
-          The best email,
-          <br /> ever made.
-        </h1>
-        <p className="mt-[17px] text-[white]/70 text-[18px] font-[family-name:var(--font-inter)] leading-[27px]">
-          Get real-time notifications with our email app, ensuring you
-        </p>
-        <p className=" text-[white] text-[18px] font-[family-name:var(--font-inter)] leading-[27px]">
-          never miss an important message.
-        </p>
-        <div className="mt-[27px] flex gap-[19px] items-center">
-          <button className="h-[65px] px-[60px] get-started-btn text-[22px] font-bold font-[family-name:var(--font-inter)] text-white">
-            Get Started
-          </button>
-          <button className="h-[65px] px-[60px] explore-btn text-[22px] font-medium font-[family-name:var(--font-inter)] text-black">
-            𝕏 Reach us
-          </button>
+        <div className=" z-10 flex flex-col items-center">
+          <h1 className="mt-[38px] font-[family-name:var(--font-montserrat)] text-[90px] text-[rgba(255,255,255,0.94)] font-black leading-[90px] text-center">
+            The best email,
+            <br /> ever made.
+          </h1>
+          <p className="mt-[17px] text-[white]/70 text-[18px] font-[family-name:var(--font-inter)] leading-[27px]">
+            Get real-time notifications with our email app, ensuring you
+          </p>
+          <p className=" text-[white] text-[18px] font-[family-name:var(--font-inter)] leading-[27px]">
+            never miss an important message.
+          </p>
+          <div className="mt-[27px] flex gap-[19px] items-center">
+            <button className="h-[65px] px-[60px] get-started-btn text-[22px] font-bold font-[family-name:var(--font-inter)] text-white">
+              Get Started
+            </button>
+            <button className="h-[65px] px-[60px] explore-btn text-[22px] font-medium font-[family-name:var(--font-inter)] text-black">
+              𝕏 Reach us
+            </button>
+          </div>
         </div>
       </div>
     </div>
